52ce900f2da47260e1c36684890eed21aa5a08a1
“Henil <henilgajjar08@gmail.com>
2024-07-15 20:14:36 -0400

    update

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import openai
 from langchain_openai import ChatOpenAI
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_groq import ChatGroq
 import os
 
 import os
@@ -12,6 +13,7 @@ load_dotenv()
 ## Langsmith Tracking
 os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
 os.environ["LANGCHAIN_PROJECT"]=os.getenv("LANGCHAIN_PROJECT")
+os.environ["GROQ_API_KEY"]=os.getenv("GROQ_API_KEY")
 os.environ["LANGCHAIN_TRACING_V2"]="true"
 
 ## Prompt Template
@@ -22,38 +24,50 @@ prompt=ChatPromptTemplate.from_messages(
     ]
 )
 
-def generate_response(question, api_key, engine, temperature, max_tokens):
-    openai.api_key=api_key
-
-    llm=ChatOpenAI(model=engine)
+def generate_response(question, engine, temperature, max_tokens, api_key=None):
+    if api_key is not None:
+        openai.api_key=api_key
+        llm=ChatOpenAI(model=engine, temperature=temperature, max_tokens=max_tokens)
+    else:
+        llm=ChatGroq(model=engine, temperature=temperature, max_tokens=max_tokens)
     output_parser=StrOutputParser()
     chain = prompt|llm|output_parser
     answer = chain.invoke({'question': question})
     return answer
 
+
 ## #Title of the app
-st.title("Enhanced Q&A Chatbot With OpenAI")
+st.title("Enhanced Q&A Chatbot With Latest LLM Models")
 
 ## Sidebar for settings
 st.sidebar.title("Settings")
 api_key=st.sidebar.text_input("Enter your Open AI API Key:", type="password")
 
 ## Select the OpenAI model
-engine=st.sidebar.selectbox("Select Open AI model", ["gpt-4o","gpt-4-turbo","gpt-4"])
+engine=st.sidebar.selectbox("Select Open AI model", ["gpt-4o","gpt-4-turbo","gpt-4", "llama3-70b-8192", "llama3-8b-8192", "gemma2-9b-it", "gemma-7b-it", "mixtral-8x7b-32768"])
 
 ## Adjust response parameter
-temperature=st.sidebar.slider("Temperature", min_value=0.0, max_value=1.0, value=0.7)
-max_tokens = st.sidebar.slider("Max Tokens", min_value=50, max_value=300, value=150)
+temperature=st.sidebar.slider("Temperature", min_value=0.0, max_value=2.0, value=0.7)
+max_tokens = st.sidebar.slider("Max Tokens", min_value=50, max_value=5000, value=500)
 
 ## Main interface for user input
 st.write("Go ahead and ask any question")
 user_input=st.text_input("You:")
 
-if user_input and api_key:
-    response=generate_response(user_input,api_key,engine,temperature,max_tokens)
-    st.write(response)
+openai_engines = ["gpt-4o","gpt-4-turbo","gpt-4"]
 
-elif user_input:
-    st.warning("Please enter the OPen AI aPi Key in the sider bar")
+if engine in openai_engines:
+    if user_input and api_key:
+        response=generate_response(user_input,engine,temperature,max_tokens,api_key=api_key)
+        st.write(response)
+    elif user_input:
+        st.warning("Please enter the OpenAI API Key in the slider bar")
+    else:
+        st.write("Please provide the user input")
 else:
-    st.write("Please provide the user input")
+    if user_input:
+        response=generate_response(user_input,engine,temperature,max_tokens)
+        st.write(response)
+    else:
+        st.write("Please provide the user input")
+    
